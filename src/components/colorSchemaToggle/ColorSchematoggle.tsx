e71a418d276116431cee0bd7873d88eab1f8e881
Neda Vaheb <n.vaheb.nv@gmail.com>
2025-11-29 12:23:51 -0800

    chore:update colorschematoggle

--- src/components/colorSchemaToggle/ColorSchematoggle.tsx
+++ src/components/colorSchemaToggle/ColorSchematoggle.tsx
@@ -1,15 +1,34 @@
-'use client';
+"use client";
+import {clsx} from "clsx";
+import {
+  ActionIcon,
+  Group,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
+import classes from "./ColorShemeToggle.module.css";
+import { MdOutlineWbSunny } from "react-icons/md";
+import { FaRegMoon } from "react-icons/fa";
 
-import { Button, Group, useMantineColorScheme } from '@mantine/core';
-
-export function ColorSchemeToggle() {
+export function ActionToggle() {
   const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+    <Group justify="center">
+      <ActionIcon
+        onClick={() =>
+          setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+        }
+        variant="default"
+        size="xl"
+        radius="md"
+        aria-label="Toggle color scheme">
+        <MdOutlineWbSunny className={clsx(classes.icon, classes.light)} />
+        <FaRegMoon className={clsx(classes.icon, classes.dark)} />
+      </ActionIcon>
     </Group>
   );
 }
